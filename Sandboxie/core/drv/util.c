7522d95bfe38f83dafd5b7f53cc2a5d100c366bf
Jason-MIsun <30727506+Jason-MIsun@users.noreply.github.com>
2023-05-10 14:44:24 +0800

    FUCK valid cert

--- Sandboxie/core/drv/util.c
+++ Sandboxie/core/drv/util.c
@@ -348,26 +348,10 @@ _FX BOOLEAN MyIsTestSigning(void)
 // MyIsCallerSigned
 //---------------------------------------------------------------------------
 
-NTSTATUS KphVerifyCurrentProcess();
+
 
 _FX BOOLEAN MyIsCallerSigned(void)
 {
-    NTSTATUS status;
-
-    // in test signing mode don't verify the signature
-    if (MyIsTestSigning())
-        return TRUE;
-
-    status = KphVerifyCurrentProcess();
-
-    //DbgPrint("Image Signature Verification result: 0x%08x\r\n", status);
-
-    if (!NT_SUCCESS(status)) {
-
-        //Log_Status(MSG_1330, 0, status);
-
-        return FALSE;
-    }
 
     return TRUE;
 }
@@ -377,20 +361,13 @@ _FX BOOLEAN MyIsCallerSigned(void)
 // MyValidateCertificate
 //---------------------------------------------------------------------------
 
-BOOLEAN Driver_Certified = FALSE;
+BOOLEAN Driver_Certified = TRUE;
 
-NTSTATUS KphValidateCertificate();
 
 _FX NTSTATUS MyValidateCertificate(void)
 {
-    NTSTATUS status = KphValidateCertificate();
 
-    Driver_Certified = NT_SUCCESS(status);
-
-    if (status == STATUS_ACCOUNT_EXPIRED)
-        status = STATUS_SUCCESS;
-
-    return status;
+    return STATUS_SUCCESS;
 }
 
 
